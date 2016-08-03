420393cbb7ca016dcb5d99030b3efa97587b8742
Gabriel Diaz <gdiaz1410@gmail.com>
2016-08-03 10:07:26 -0500

    Added hw

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,28 +1,38 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
+
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
-  var cards = []; // deck
+  var cards = [];
+ // deck
   var shuffledCards = []; // deck shuffled
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+    for (var i = 0; i < suits.length; i++) {
+             // console.log(suits[i]);
+            for(var j=0; j<values.length;j++) {
+                // console.log(values[j]);
+                // console.log(suits[i], values[j]);
+
+                var closer = {suit: suits[i], value: values[j]};
 
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+
+                cards.push(closer);
 
 
-  // Pull the top card from the newly shuffledCards
+            }
 
 
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
+    }
 
+    console.log(cards);
 }
+
+
+deck_o_cards();
+
 
 
 

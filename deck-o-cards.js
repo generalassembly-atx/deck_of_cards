19ce33aae7f335a995009ff63ae520443c798bd3
Richard Davis <rddavistx@hotmail.com>
2016-08-04 21:27:09 -0500

    deck-o-cards homework

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,49 +1,54 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+var deck = [];
+
 function deck_o_cards() {
+
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
-  var cards = []; // deck
-  var shuffledCards = []; // deck shuffled
-
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
-
-
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
-
-
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
-}
-
-
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
-function shuffle(array) {
-    var counter = array.length, temp, index;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+  for (var i = 0; i < suits.length; i++) {
+    var suit = suits[i];
+    for (var j = 0; j < values.length; j++) {
+      var value = values[j];
+      var card = {
+        suit: suit,
+        value: value
+      };
+      deck.push(card);
     }
-
-    return array;
+  }
+  return deck;
 }
+
+var d = deck_o_cards();
+console.log(d);
+
+function shuffle(array) {
+  var counter = array.length,
+    temp, index;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+
+  return array;
+}
+var shuffledCards = shuffle(deck);
+
+
+console.log(shuffledCards);
+var topCard = shuffledCards[0];
+console.log(shuffledCards[0]);
+
+console.log("The deck has " + deck.length + " cards");
+console.log("The top card is " + topCard.value + " of " + topCard.suit);

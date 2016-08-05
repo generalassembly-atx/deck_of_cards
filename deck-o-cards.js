80319b06490ce4603ee9c6b2ab1fd19c43cdebe5
Shirish Dali <designsbydali@gmail.com>
2016-08-05 09:11:23 -0500

    decOcardsHW submissions

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,6 +1,7 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
+
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
@@ -10,40 +11,71 @@ function deck_o_cards() {
   // Make 52 card objects and store them in the "cards" array
   // Hint: use 2 for loops
 
+  for (var i = 0; i < suits.length; i++) {
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+    for (var j = 0; j < values.length; j++) {
 
+      var closer = {
+        suit: suits[i],
+        value: values[j]
+      };
 
-  // Pull the top card from the newly shuffledCards
+      cards.push(closer);
 
+    }
 
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
+  }
+
+  console.log(cards);
+
+shuffle();
 
 }
+
+deck_o_cards();
+
+
+
+// 2. Shuffle the cards
+// Hint: shuffle function is already defined below
+
+
+// shuffle the deck
+// shuffle();
+
+
+// Pull the top card from the newly shuffledCards
+
+
+// 3. Print the results:
+// "The deck has {n} cards"
+// "The top card is the {value} of {suit}"
+
+
 
 
 
 // Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
-function shuffle(array) {
-    var counter = array.length, temp, index;
+function shuffle(cards) {
+  var counter = cards.length,
+    temp, index;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+    // Decrease counter by 1
+    counter--;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+    // And swap the last element with it
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
 
-    return array;
+  return array;
 }
+
+shuffle();

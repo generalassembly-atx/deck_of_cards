e71557e184212148222d163a968d47f24a6c291c
jesse houghton <OiseauNoir>
2016-08-10 18:13:30 -0500

    finished shuffling deck

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,62 +1,42 @@
+var deck = [];
+
 function deck_o_cards() {
-
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-
-  var cards = []; // deck
+  var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
   var shuffledCards = []; // deck shuffled
-
-    for (var i = 0; i < suits.length; i++) {
-      //console.log(suits[i]);
-    	for (var j = 0; j < values.length; j++) {
-      		//console.log(values[j]);
-      		//console.log(suits[i], values[j]);
-		    var object = {suit: suits[i], value: values[j]};
-			cards.push(object);
-    	}
-   	}
-	console.log(cards)
+  for (var i = 0; i < suits.length; i++) {
+    //console.log(suits[i]);
+    for (var j = 0; j < values.length; j++) {
+  		//console.log(values[j]);
+  		//console.log(suits[i], values[j]);
+	    var cards = {suit: suits[i], value: values[j]};
+		  deck.push(cards);
+  	}
+ 	}
+	return deck;
 }
-
 deck_o_cards()
 
-
-
-
-
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
-
-
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
-
-
-
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
 function shuffle(array) {
     var counter = array.length, temp, index;
-
-    // While there are elements in the array
     while (counter > 0) {
-        // Pick a random index
         index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
         counter--;
-
-        // And swap the last element with it
         temp = array[counter];
         array[counter] = array[index];
         array[index] = temp;
     }
-
     return array;
 }
+
+//shuffled deck
+var shuffledCards = shuffle(deck);
+console.log(shuffledCards)
+
+//top card
+var topCard = shuffledCards[0];
+console.log(topCard);
+
+//console log statements
+console.log("the deck has "+ deck.length +"cards");
+console.log("the top card is the " + topCard.value +" of " + topCard.suit);

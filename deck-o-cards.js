1f3fdad7456528103c83895010401e921bfdae82
niran manandhar <nirnaism@gmail.com>
2016-09-05 21:01:12 -0500

    called the shuffle function and deleted err syn

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -7,62 +7,65 @@ function deck_o_cards() {
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
 
-  for(var i = 0; i < suits.length; i++) {
-  	// console.log(suits[i]);
-  		for(var j = 0; j < values.length; j++) {
-  			// console.log(values[j]);
-  		// console.log(suits[i], suits[j]);
+  for (var i = 0; i < suits.length; i++) {
+    // console.log(suits[i]);
+    for (var j = 0; j < values.length; j++) {
+      // console.log(values[j]);
+      // console.log(suits[i], suits[j]);
 
-  	var closer = {
-  		suit: suits[i],
-  		value: values[j]
+      var closer = {
+        suit: suits[i],
+        value: values[j]
 
-  	};
+      };
 
-  	cards.push(closer);
-  		}
-	}
-	console.log(cards);
+      cards.push(closer);
+    }
+  }
+  console.log(cards);
 }
 deck_o_cards();
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
+// Make 52 card objects and store them in the "cards" array
+// Hint: use 2 for loops
 
 
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
+// 2. Shuffle the cards
+// Hint: shuffle function is already defined below
 
 
-  // Pull the top card from the newly shuffledCards
+// Pull the top card from the newly shuffledCards
 
 
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
+// 3. Print the results:
+// "The deck has {n} cards"
+// "The top card is the {value} of {suit}"
 
-}
+
 
 
 
 // Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
 function shuffle(cards) {
-    var counter = cards.length, temp, index;
+  var counter = cards.length,
+    temp, index;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+    // Decrease counter by 1
+    counter--;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+    // And swap the last element with it
+    temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
 
-    return array;
+  return array;
 }
+
+shuffle();

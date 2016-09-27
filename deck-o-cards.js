c4dcf11247b72ddd92a4bdb1e67f88623d761712
jordan wade <jordanwade2009@live.com>
2016-09-27 18:42:26 -0500

    completed deck

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,11 +1,8 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
-function deck_o_cards() {
-  var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-
-  var cards = []; // deck
-  var shuffledCards = []; // deck shuffled
+// for (prop in wdiStudent) {
+//   console.log("wdiStudent key: " + prop + " = " + wdiStudent[prop]);
+// }
 
   // Make 52 card objects and store them in the "cards" array
   // Hint: use 2 for loops
@@ -22,28 +19,50 @@ function deck_o_cards() {
   // "The deck has {n} cards"
   // "The top card is the {value} of {suit}"
 
+function deck_o_cards() {
+  var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  var cards = []; // deck
+  var shuffledCards = []; // deck shuffled
+
+  for (var i = 0; i < suits.length; i++) {
+    var card = {};
+    for (var j = 0; j < values.length; j++) {
+      // card.suit = suits[i];
+      // card['value'] = values[j];
+      card = {
+        suit: suits[i],
+        value: values[j]
+      };
+      cards.push(card);
+      // console.log(card);
+    }
+  }
+  var shuffledDeck = shuffle(cards);
+  console.log("The deck has " + cards.length + " cards. The top card is the " + cards[0].value + " of " + cards[0].suit + ".");
 }
-
-
 
 // Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
 function shuffle(array) {
-    var counter = array.length, temp, index;
+  var counter = array.length, temp, index;
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
+  // While there are elements in the array
+  while (counter > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * counter);
 
-        // Decrease counter by 1
-        counter--;
+      // Decrease counter by 1
+      counter--;
 
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
+      // And swap the last element with it
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
 
-    return array;
+  return array;
 }
+
+deck_o_cards();

c9f915a241e0b04b57a7938f8f311af05518cd7b
sean lewis <seanmartinlewis@gmail.com>
2016-09-27 22:30:30 -0500

    The deck of cards homework.

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,5 +1,4 @@
-// 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
-// Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+
 function deck_o_cards() {
   var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
   var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
@@ -7,43 +6,29 @@ function deck_o_cards() {
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
 
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
-
-
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
-
-
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
-}
-
-
-
-// Fisher-Yates Shuffle
-// http://stackoverflow.com/a/6274398
-function shuffle(array) {
-    var counter = array.length, temp, index;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+  for (var s = 0; s < suits.length; s++) {
+    //console.log(values[i]);
+  for (var v = 0; v < values.length; v++) {
+      cards = (v+1, 'suits: ' + suits[s] + ' , ' + 'value: ' + values[v]);
+      console.log(cards);
     }
+  }
+  function shuffle(cards) {
+      var counter = cards.length, temp, index;
+      while (counter > 0) {
+          index = Math.floor(Math.random() * counter);
+          counter--;
+          temp = cards[counter];
+          cards[counter] = cards[index];
+          cards[index] = temp;
 
-    return array;
+      }
+      return cards;
+
+
+  }
+  console.log('The deck has ' + cards.length + ' cards');
+  console.log("The two top cards = " + cards);
 }
+
+deck_o_cards();

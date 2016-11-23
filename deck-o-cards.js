6937ba1bd426566e8e8ec5e9af5c917d5a2a2c42
Kyle Crichton <kylecrichton@gmail.com>
2016-11-22 22:33:41 -0600

    I definitely need help with this.

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,30 +1,25 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
-function deck_o_cards() {
-  var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-
-  var cards = []; // deck
-  var shuffledCards = []; // deck shuffled
-
-  // Make 52 card objects and store them in the "cards" array
-  // Hint: use 2 for loops
-
-
-  // 2. Shuffle the cards
-  // Hint: shuffle function is already defined below
-
-
-  // Pull the top card from the newly shuffledCards
-
-
-  // 3. Print the results:
-  // "The deck has {n} cards"
-  // "The top card is the {value} of {suit}"
-
+function card(values, name, suit){
+	this.values = values;
+	this.name = name;
+	this.suit = suit;
 }
 
+function deck_o_cards() {
+  this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  this.suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
+  var cards = []; // deck
+  // Make 52 card objects and store them in the "cards" array
+  // Hint: use 2 for loops
+  for (var i=0; i< this.values.length; i++) {
+    for(var n=0; n< this.suits.length; n++) {
+      cards.push(new card( i+1, this.values[i], this.suits[n]));
+    }
+  }
+  return cards;
+}
 
 // Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
@@ -47,3 +42,4 @@ function shuffle(array) {
 
     return array;
 }
+      console.log(deck_o_cards());

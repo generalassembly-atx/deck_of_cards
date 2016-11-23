010d6ec96ad441e16340067645028c14d78e673f
dan-nguyen <dtn601@gmail.com>
2016-11-22 23:33:34 -0600

    finished assignment

--- deck-o-cards.js
+++ deck-o-cards.js
@@ -1,29 +1,51 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays.
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
+function card(values,suits){
+this.values = values;
+this.suits = suits;
+}
+
 function deck_o_cards() {
-  var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+  this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  this.suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
   var cards = []; // deck
   var shuffledCards = []; // deck shuffled
+  
+
 
   // Make 52 card objects and store them in the "cards" array
   // Hint: use 2 for loops
 
 
+for (var i=0;i < this.values.length;i++){
+  for(var n=0;n < this.suits.length;n++){
+    cards.push(new card(this.values[i],this.suits[n]));
+  }
+}
+  return cards;
+}
+
+cards = deck_o_cards();
+
+
+
   // 2. Shuffle the cards
   // Hint: shuffle function is already defined below
+shuffledCards = shuffle(cards);
 
 
   // Pull the top card from the newly shuffledCards
+var printCards = shuffledCards[0];
+
 
 
   // 3. Print the results:
   // "The deck has {n} cards"
   // "The top card is the {value} of {suit}"
 
-}
-
+console.log("This deck has " + cards.length + " cards");
+console.log("The top card is the " + printCards.values + " of " + printCards.suits);
 
 
 // Fisher-Yates Shuffle
